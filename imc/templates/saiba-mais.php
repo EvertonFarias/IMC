95e6cb7347c6f10f2f928829621a39454f9f0fe4
EvertonFarias <143721223+EvertonFarias@users.noreply.github.com>
2024-03-27 08:26:24 -0300

    Update saiba-mais.php

--- imc/templates/saiba-mais.php
+++ imc/templates/saiba-mais.php
@@ -1,49 +1,24 @@
-<?php 
-
-
-
-?>
-
-<html>
+  <!DOCTYPE html>
+  <html lang="en">
   <head>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-
-
-      function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Gordura', 25],
-          ['Massa Magra e Resíduos Corporais', 75],
-
-        ]);
-
-        // Set chart options
-        var options = {'title':'Seu Dados Corporais:',
-                       'width':600,
-                       'height':600,
-                       'backgroundColor': 'transparent'};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saiba mais</title>
+    <link rel="stylesheet" href="./saibamias.css"
   </head>
-
   <body>
-    <!--Div that will hold the pie chart-->
-    <div id="chart_div"></div>
+    <h1>
+      Saiba mais
+    </h1>
+    
+    <h2>
+    Nosso objetivo foi criar um algoritmo que mostre o IMC do individuo, juntamente com os seus percentuais corporais para maior compreensão dos seus dados corporais!
+    Atualmente vivemos na Geração Saúde, onde grande parte da população busca alternativas saudáveis como treinos funcionais, academia e exercícios ao ar livre como caminhada ou corridas. 
+    Com isso surge um grande duvida: “ Como estão os meus Níveis Corporais? “.  Com essa duvida, a principal solução encontrada por grande parte das pessoas é  fazer uma avaliação corporal com algum profissional da área.
+    Porém existe um problema, essas avaliações feitas por profissionais, as vezes se tornam inviáveis financeiramente, com valores que giram em torno de R$80 a R$150 dependendo do profissional.
+    Com base nessa problemática,  criamos um método para que as pessoas consigam saber seus níveis corporais sem a necessidade de pagar um profissional.
+
+    </h2>
+    
   </body>
-</html>
+  </html>
